00d209739001889742b80f1a8e0b55cfd5a26abf
Max_Kr <m.krymov@mail.ru>
2024-04-28 12:26:27 +0300

    Tproger Task_01

---
+++ task_01.py
@@ -0,0 +1,24 @@
+'''
+Есть список a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89].
+Выведите все элементы, которые меньше 5.
+'''
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for el in a:
+    if el < 5:
+        print(el)
+        
+        
+'''
+echo "# 90_Tproger" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Krymov-Maksim/90_Tproger.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/Krymov-Maksim/90_Tproger.git
+git branch -M main
+git push -u origin main
+'''
